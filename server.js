6b5f97e3fbd350fb840a743656365e9dec3a780b
Mrinal Choudhary <mrinal7331@gmail.com>
2024-07-28 12:14:26 +0530

    added CORS for Deployement

--- server.js
+++ server.js
@@ -4,6 +4,24 @@ const express = require("express");
 
 const app = express();
 
+// CORS added for Deployement purpose
+const cors = require("cors");
+
+// const allowedOrigins = ["https://mrinal-choudhary.netlify.app"];
+const allowedOrigins = ["https://frontendv2-5yzhcce4v-mrinal07s-projects.vercel.app"];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+// CORS added for Deployement purpose
+
 const dbConfig = require("./config/dbConfig");
 
 
