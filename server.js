074da8d3ae09631df45b894ed52ea800aa9d21a8
Mrinal Choudhary <mrinal7331@gmail.com>
2024-07-28 20:45:24 +0530

    update code related to CORS

--- server.js
+++ server.js
@@ -6,20 +6,7 @@ const app = express();
 
 // CORS added for Deployement purpose
 const cors = require("cors");
-
-// const allowedOrigins = ["https://mrinal-choudhary.netlify.app"];
-const allowedOrigins = ["https://frontendv2-5yzhcce4v-mrinal07s-projects.vercel.app"];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = "The CORS policy for this site does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 // CORS added for Deployement purpose
 
 const dbConfig = require("./config/dbConfig");
@@ -30,7 +17,9 @@ const portfolioRoute = require("./routes/portfolioRoute");
 app.use(express.json());
 app.use("/api/portfolio", portfolioRoute);
 
-const port = process.env.port || 5000;
+const port = process.env.PORT
+
+|| 5000;
 app.listen(port, () => {
   console.log("Server running on port on port " + port);
 });
