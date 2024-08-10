9c7efbd92b08575240e49e50f9332cd6ba0bfd32
Mrinal Choudhary <mrinal7331@gmail.com>
2024-08-10 12:08:10 +0530

    removed tinyurl

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 require("dotenv").config();
-
 const express = require("express");
-const path = require('path'); // Import the path module
-
 const app = express();
 
 // CORS added for Deployement purpose
@@ -16,15 +13,8 @@ const portfolioRoute = require("./routes/portfolioRoute");
 
 app.use("/api/portfolio", portfolioRoute);
 
-const redirectURL = require("./controllers/route.js")
-app.use("/",redirectURL)
+const port = process.env.PORT || 5000;
 
-
-
-
-const port = process.env.PORT
-
-|| 5000;
 app.listen(port, () => {
   console.log("Server running on port on port " + port);
 });
